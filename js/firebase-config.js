a1c35c194a0ada81d1e25f854538fde0b017bd98
warmmcat <warmmcat@gmail.com>
2026-08-23 10:30:21 +0800

    Configure Firebase web app

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,10 @@
 export const FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDhFsLF2HhL7kPqoHJ6-Balghco67tmaL8",
+  authDomain: "student-support-web-v2.firebaseapp.com",
+  projectId: "student-support-web-v2",
+  storageBucket: "student-support-web-v2.firebasestorage.app",
+  messagingSenderId: "145144017790",
+  appId: "1:145144017790:web:e0dc20c28d073793b7392f"
 };
 
 export const APP_SETTINGS = {
