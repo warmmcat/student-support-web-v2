3b525164f4529f5be7d02711ea5f44b6f48b7d93
warmmcat <warmmcat@gmail.com>
2026-08-23 10:12:23 +0800

    Add Firebase configuration template

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+export const APP_SETTINGS = {
+  adminPath: "admin.html",
+  maxQuestionLength: 5000
+};
